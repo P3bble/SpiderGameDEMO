85b816f59ad84e10440e60c5802d5f74864e5fb7
P3bble <isaac8950@hotmail.co.uk>
2024-11-14 09:22:27 +0000

    triangle update

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
         //input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
+        //animation
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
