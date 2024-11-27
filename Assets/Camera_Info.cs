fe74593005bebf90205dc423da4ef32882d3b388
P3bble <isaac8950@hotmail.co.uk>
2024-11-27 15:55:09 +0000

    Health update!

--- Assets/Camera_Info.cs
+++ Assets/Camera_Info.cs
@@ -14,10 +14,5 @@ public class Camera_Controller : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y,transform.position.z);
-
-
-    }
+    void Update() => transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
 }
---
+++ Assets/Player_Health.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
+
+public class Player_Health : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 10;
+    public Text HEALTH;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HEALTH.text = "Health: " + health.ToString();
+
+    }
+    public void TakeDamage(int ammount)
+    {
+        health -= ammount;
+        if (health <= 0 )
+        {
+            Destroy(gameObject);
+        }
+    }
+}
