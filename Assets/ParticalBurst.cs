5dc0d1b2015e2b7ca0e64b69fdd8e2cb317f791f
P3bble <isaac8950@hotmail.co.uk>
2024-11-28 10:13:15 +0000

    coin partical system is refined

--- Assets/ParticalBurst.cs
+++ Assets/ParticalBurst.cs
@@ -6,27 +6,29 @@ using UnityEngine;
 public class ParticalBurst : MonoBehaviour
 {
 
-    public ParticleSystem collisionParticalSystem;
-    public SpriteRenderer sr;
+    public ParticleSystem collisionParticalSystem; // this gets the correct partical system (can put this in the script to connect)
+    public SpriteRenderer spriteofpartical; // this gets the sprite renderer (of coins (spider eggs))
     public bool once = true;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player") && once) {
+
+            // collision between the player, as tagged, and the object which the script is on (spider egg)
           
             var em = collisionParticalSystem.emission;
-            var dur = collisionParticalSystem.duration;
+            var dur = collisionParticalSystem.main.duration; // duration can be changed here
 
             em.enabled = true;
-            collisionParticalSystem.Play();
+            collisionParticalSystem.Play(); // once collided then play system, change to false
 
-            once = false;
-            Destroy(sr);
+            once = false; // once false, destroys partical, called to destroy the object
+            Destroy(spriteofpartical);
             Invoke(nameof(DestroyObj), dur);
 
         }
     }
 
-    void DestroyObj()
+    void DestroyObj() // destroys object
     {
         Destroy(gameObject);
     }
